0baf3fb505721d0c51c3c54515178fb60e027782
rehan-sameer <s202463580@kfupm.edu.sa>
2025-04-29 10:47:00 +0300

    setters and getters for user

--- models/User.java
+++ models/User.java
@@ -1,11 +1,12 @@
 package models;
 
-  public class User {
+public class User {
     private String name;
     private String email;
     private String username;
     private String password;
 
+    // Constructor
     public User(String name, String email, String username, String password) {
         this.name = name;
         this.email = email;
@@ -13,7 +14,39 @@ package models;
         this.password = password;
     }
 
+    // getters for the user
 
-    
-}
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Ssetters for the user
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
