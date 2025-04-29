b212af35b52ffebb28cfa5e0dd08d42f471b2995
rehan-sameer <s202463580@kfupm.edu.sa>
2025-04-29 10:23:16 +0300

    basic features of a user

--- models/User.java
+++ models/User.java
@@ -1,4 +1,19 @@
 package models;
 
-public class User {
+  public class User {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+
+    public User(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters (you can auto-generate them in IntelliJ)
+    
+}
 }
