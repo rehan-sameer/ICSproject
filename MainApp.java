3c29632de3f0c6df63fa1e6effb76b3f534d8566
rehan-sameer <s202463580@kfupm.edu.sa>
2025-04-30 16:31:11 +0300

    random updates

--- MainApp.java
+++ MainApp.java
@@ -1,2 +1,26 @@
-public class MainApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Welcome to Classroom Reservation System!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Classroom Reservation System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
